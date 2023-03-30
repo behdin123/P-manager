adab5705a973c1c1ee5c5d40c8c321ce8fb54b64
behdin123 <behdin2010@gmail.com>
2023-03-30 12:22:41 +0200

    new comments that explains the routes for the project.js

--- app/router/project.js
+++ app/router/project.js
@@ -8,12 +8,25 @@ const { mongoIDValidator } = require("../validations/public");
 const { addStrToArr } = require("../middlewares/convertStringToArray");
 const router = require("express").Router();
 
+// Create a new project
 router.post("/create",fileupload(), checkLogin, addStrToArr("tags"), uploadFile, createProjectValidator(),expressValidatorMapper, ProjectController.createProject)
+
+// Get a list of all projects
 router.get("/list", checkLogin, ProjectController.getAllProject)
+
+// Get a specific project by ID
 router.get("/:id", checkLogin, mongoIDValidator(), expressValidatorMapper, ProjectController.getProjectById)
+
+// Remove a project by ID
 router.delete("/remove/:id", checkLogin,mongoIDValidator(), expressValidatorMapper, ProjectController.removeProject)
+
+// Update a project by ID
 router.put("/edit/:id", checkLogin, mongoIDValidator(), expressValidatorMapper,ProjectController.updateProject)
+
+// Update a project's image by ID
 router.patch("/edit-projectImage/:id", fileupload(), checkLogin,uploadFile, mongoIDValidator(), expressValidatorMapper,ProjectController.updateProjectImage)
+
+// Export the router as a module
 module.exports = {
     projectRoutes : router
 }

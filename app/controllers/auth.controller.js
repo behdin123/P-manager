1d399ddb959812fa6114158027d1d8b7e1254949
behdin123 <behdin2010@gmail.com>
2023-05-23 09:02:17 +0200

    render upload

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -82,7 +82,6 @@ class AuthController{
                 sameSite: 'none',
                 secure: true,
                 maxAge: 24 * 60 * 60 * 1000, // 24 hours
-                sameSite: 'None',
             });
 
             // Send a response with the token to the user

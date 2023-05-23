297bf567b9134a1d5af56e4173c666e80920d62b
behdin123 <behdin2010@gmail.com>
2023-05-23 08:39:55 +0200

    token domain added

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -82,6 +82,7 @@ class AuthController{
                 sameSite: 'none',
                 secure: true,
                 maxAge: 24 * 60 * 60 * 1000, // 24 hours
+                domain: piratelifesmatter.org,
             });
 
             // Send a response with the token to the user
